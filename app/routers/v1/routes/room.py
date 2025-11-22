97346800a6d13eb68f4db0c7460b84315948770b
IsaiahSama <zeldaplayergl@gmail.com>
2025-11-22 08:50:20 -0400

    will now retrieve rooms

--- app/routers/v1/routes/room.py
+++ app/routers/v1/routes/room.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
+from app.db.mongo_client import MongoORM
 from app.schemas import RoomSuccessResponse
 from app.schemas.failure_response import FailureResponse
 
 room_router = APIRouter(prefix="/rooms")
 
 
-@room_router.get("/", response_model=RoomSuccessResponse, responses={404: {"model": FailureResponse}})
-async def get_rooms() -> RoomSuccessResponse:
-    pass  # DB call
+@room_router.get(
+    "/", response_model=RoomSuccessResponse, responses={404: {"model": FailureResponse}}
+)
+async def get_rooms(request: Request) -> RoomSuccessResponse:
+    db: MongoORM = request.app.state.db
+    rooms = await db.get_all_rooms()
+    room_names = [room.room for room in rooms]
+    return RoomSuccessResponse(data=room_names)
